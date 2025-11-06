2ce08efd1f854fde54267d890e8986ce4519fd70
hsfar <hsfar@hsfarui-MacBookPro.local>
2025-11-07 03:00:57 +0900

    change server settings

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const SECRET_KEY = "supersecretkey"; // JWT 서명용 (원하면 더 복잡하게)
 
+app.listen(PORT, () => console.log(`✅ 서버 실행 중: ${PORT}`));
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
